84da8c03b6591bfd9eb410ee144772f62a40f0fe
GauravSaini104 <gaurav-saini@pearlorganisation.com>
2026-07-25 17:53:53 +0530

    ok

--- models/user.model.js
+++ models/user.model.js
@@ -49,6 +49,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: ''
   },
+  phone: {
+    type: String,
+    default: ''
+  },
   livingIn: {
     type: String,
     default: ''
